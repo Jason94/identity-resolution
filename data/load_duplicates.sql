fc790fe0cfc565d1ed501ca1acc67dc6ea577f5f
Jason Walker <jason@indivisible.org>
2023-07-07 00:37:07 -0400

    Add state field to data

--- data/load_duplicates.sql
+++ data/load_duplicates.sql
@@ -19,21 +19,37 @@ WITH ea_emails AS (
         WHERE 
             es.dateunsubscribed IS NULL
     ) e
-    ON 
-        c.vanid = e.vanid
-    WHERE 
-        e.rn = 1
+    ON c.vanid = e.vanid
+    WHERE e.rn = 1
+), ea_addrs AS (
+    SELECT
+        c.vanid,
+        a.state
+    FROM
+        indivisible_ea.contacts_iv c
+    JOIN (
+        SELECT
+            vanid,
+            a.state,
+            ROW_NUMBER() OVER(PARTITION BY vanid ORDER BY datemodified DESC) as rn
+        FROM
+            indivisible_ea.contactsaddresses_iv a
+    ) a
+        ON a.vanid = c.vanid
+        WHERE a.rn = 1
 ), ea_xwalks AS (
     SELECT
         voterbase_id as xwalk_id,
         LOWER(COALESCE(ea_c.firstname, '')) AS first_name_field,
         LOWER(COALESCE(ea_c.lastname, '')) AS last_name_field,
         LOWER(COALESCE(ea_emails.email, '')) AS email_field,
-        first_name_field || '|' || last_name_field || '|' || email_field as comp_string
+        LOWER(COALESCE(a.state, '')) as state_field
     FROM
         indivisible_ea.contacts_iv ea_c
     LEFT JOIN ea_emails
         ON ea_emails.vanid = ea_c.vanid
+    LEFT JOIN ea_addrs a
+        ON a.vanid = ea_c.vanid
     JOIN indivisible_infrastructure.xwalk x
         ON ea_c.vanid = x.everyaction_id
 ), ak_xwalks AS (
@@ -42,7 +58,7 @@ WITH ea_emails AS (
         LOWER(COALESCE(ak.first_name, '')) AS first_name_field,
         LOWER(COALESCE(ak.last_name, '')) AS last_name_field,
         LOWER(COALESCE(ak.email, '')) AS email_field,
-        first_name_field || '|' || last_name_field || '|' || email_field as comp_string
+        LOWER(COALESCE(ak.state, '')) as state_field
     FROM
         indivisible_ak.core_user ak
     JOIN indivisible_infrastructure.xwalk x
@@ -53,7 +69,7 @@ WITH ea_emails AS (
         LOWER(COALESCE(mc.first_name, '')) AS first_name_field,
         LOWER(COALESCE(mc.last_name, '')) AS last_name_field,
         LOWER(COALESCE(mc.email, '')) AS email_field,
-        first_name_field || '|' || last_name_field || '|' || email_field as comp_string
+        LOWER(COALESCE(mc.address_state, '')) as state_field
     FROM
         in_mobilecommons.profiles mc
     JOIN indivisible_infrastructure.xwalk x
@@ -64,31 +80,38 @@ WITH ea_emails AS (
         LOWER(COALESCE(ab.firstname, '')) AS first_name_field,
         LOWER(COALESCE(ab.lastname, '')) AS last_name_field,
         LOWER(COALESCE(ab.email, '')) AS email_field,
-        first_name_field || '|' || last_name_field || '|' || email_field as comp_string
+        LOWER(COALESCE(ab.state, '')) as state_field
     FROM
         tmc_ab.in_donations ab
     JOIN indivisible_infrastructure.xwalk x
         ON ab.ordernumber = x.actblue_id
 ), combined_xwalks AS (
-        SELECT *
-        FROM ea_xwalks
-    UNION ALL
-        SELECT *
-        FROM ak_xwalks
-    UNION ALL
-        SELECT *
-        FROM mc_xwalks
-    UNION ALL
-        SELECT *
-        FROM ab_xwalks
+    SELECT 
+        *,
+        first_name_field || '|' || last_name_field || '|' || email_field || '|' || state_field as comp_string
+    FROM (
+            SELECT *
+            FROM ea_xwalks
+        UNION ALL
+            SELECT *
+            FROM ak_xwalks
+        UNION ALL
+            SELECT *
+            FROM mc_xwalks
+        UNION ALL
+            SELECT *
+            FROM ab_xwalks
+    )
 )
 SELECT distinct
     c1.first_name_field AS first_name1, 
     c1.last_name_field AS last_name1,
     c1.email_field AS email1,
+    c1.state_field AS state1,
     c2.first_name_field AS first_name2,
     c2.last_name_field AS last_name2,
     c2.email_field AS email2,
+    c2.state_field AS state2,
     1 as label
 FROM 
     combined_xwalks c1
--- data/load_uniques.sql
+++ data/load_uniques.sql
@@ -24,19 +24,38 @@ with ea_emails AS (
         c.vanid = e.vanid
     WHERE 
         e.rn = 1
+), ea_addrs AS (
+    SELECT
+        c.vanid,
+        a.state
+    FROM
+        indivisible_ea.contacts_iv c
+    JOIN (
+        SELECT
+            vanid,
+            a.state,
+            ROW_NUMBER() OVER(PARTITION BY vanid ORDER BY datemodified DESC) as rn
+        FROM
+            indivisible_ea.contactsaddresses_iv a
+    ) a
+        ON a.vanid = c.vanid
+        WHERE a.rn = 1
 ), distincts as (
     WITH ea_c AS (
         SELECT 
             x.tmc_person_id,
             LOWER(e.firstname) AS ea_first_name, 
             LOWER(e.lastname) AS ea_last_name,
-            LOWER(ea_emails.email) AS ea_email,
+            LOWER(COALESCE(ea_emails.email, '')) AS ea_email,
+            LOWER(COALESCE(ea_addrs.state, '')) as ea_state,
             ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rownum
         FROM indivisible_infrastructure.xwalk x
         INNER JOIN indivisible_ea.contacts_iv e
             ON e.vanid = x.everyaction_id
         LEFT JOIN ea_emails
             ON ea_emails.vanid = e.vanid
+        LEFT JOIN ea_addrs
+            ON ea_addrs.vanid = e.vanid
     ),
     ak_u AS (
         SELECT 
@@ -44,6 +63,7 @@ with ea_emails AS (
             LOWER(a.first_name) AS ak_first_name, 
             LOWER(a.last_name) AS ak_last_name,
             LOWER(a.email) AS ak_email,
+            LOWER(COALESCE(a.state, '')) as ak_state,
             ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS rownum
         FROM indivisible_infrastructure.xwalk x
         INNER JOIN indivisible_ak.core_user a
@@ -53,9 +73,11 @@ with ea_emails AS (
         ea_c.ea_first_name as first_name1, 
         ea_c.ea_last_name as last_name1,
         ea_c.ea_email as email1,
+        ea_c.ea_state as state1,
         ak_u.ak_first_name as first_name2, 
         ak_u.ak_last_name as last_name2,
-        ak_u.ak_email as email2
+        ak_u.ak_email as email2,
+        ak_u.ak_state as state2
     FROM ea_c
     INNER JOIN ak_u
         ON ea_c.rownum = ak_u.rownum
@@ -65,7 +87,7 @@ with ea_emails AS (
 ), distincts_final as (
     SELECT *, -1 as label
     FROM distincts
-    LIMIT 30000
+    LIMIT 37000
 )
 select *
 from distincts_final
